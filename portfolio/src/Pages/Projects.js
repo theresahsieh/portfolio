8efeb0cbf5ccfc265aa5c9e59ceb7729289c5f57
Theresa Hsieh <theresaying.hsieh@gmail.com>
2020-12-17 20:24:47 -0500

    fixed projects

--- portfolio/src/Pages/Projects.js
+++ portfolio/src/Pages/Projects.js
@@ -17,7 +17,7 @@ const useStyles = makeStyles({
         margin: "0 0 1.5rem 1.5rem",
     },
     projectName: {
-      fontWeight:"bold"
+        fontWeight: "bold",
     },
     mainCard: {
         "@media (min-width: 960px)": {
@@ -27,7 +27,7 @@ const useStyles = makeStyles({
         borderRadius: 10,
         marginBottom: "1.5rem",
         transition: "all 0.3s ease-in-out 0s",
-        boxShadow:"0px 2px 4px -1px rgba(0,0,0,0.08)",
+        boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.08)",
         "&:hover": {
             boxShadow: "rgba(0, 0, 0, 0.22) 0px 19px 43px",
             transform: "translate3d(2px, -1px, 0px)",
@@ -40,29 +40,80 @@ const useStyles = makeStyles({
         height: "30vw",
     },
     cardChips: {
-      display: "flex",
-      float: 'right'
-    }
+        display: "flex",
+        float: "right",
+    },
 });
 
 const chipStyle = {
-  padding:"3px 6px 3px 6px",
-  borderRadius:"6px",
-  fontSize:'0.775rem',
-  fontWeight:'bold',
-}
-const klixChip = {...chipStyle, backgroundColor:"#8576ed"}
-const pecssChip = {...chipStyle, backgroundColor:"#54D0E0"}
-const yearChip = {...chipStyle, backgroundColor:'#E5E5E5'}
-const peqChip = {...chipStyle, backgroundColor:"#FF4BAF"}
-const reflectChip = {...chipStyle, backgroundColor:"#2993FC"}
-
+    padding: "3px 6px 3px 6px",
+    borderRadius: "6px",
+    fontSize: "0.775rem",
+    fontWeight: "bold",
+};
+const klixChip = { ...chipStyle, backgroundColor: "#8576ed" };
+const pecssChip = { ...chipStyle, backgroundColor: "#54D0E0" };
+const yearChip = { ...chipStyle, backgroundColor: "#E5E5E5" };
+const peqChip = { ...chipStyle, backgroundColor: "#FF4BAF" };
+const reflectChip = { ...chipStyle, backgroundColor: "#2993FC" };
 
 const Projects = () => {
     const classes = useStyles();
+
+    const projects = [
+        {
+            name: "PECSS Clinician Dashboard",
+            image: PECSSCover,
+            description:
+                "Prolonged Exposure Collective Sensing System.Improving PTSD treatment for veterans.",
+            chips: (
+                <CardActions className={classes.cardChips}>
+                    <span style={pecssChip}>UX Design + Angular JS</span>
+                    <span style={yearChip}>2019 - present</span>
+                </CardActions>
+            ),
+        },
+        {
+            name: "PEQ Widget",
+            image: PEQCover,
+            description:
+                "Patient Engagement Quotient. Visualizing electronic personal health information to improve patient adherence to treatment protocols.",
+            chips: (
+                <CardActions className={classes.cardChips}>
+                    <span style={peqChip}>UX Research + Design</span>
+                    <span style={yearChip}>2020 - present</span>
+                </CardActions>
+            ),
+        },
+        {
+            name: "Klix",
+            image: KlixCover,
+            description:
+                "Helping gamers find friends and schedule gameplay in a safe environment.",
+            chips: (
+                <CardActions className={classes.cardChips}>
+                    <span style={klixChip}>UX Research + Design</span>
+                    <span style={yearChip}>2019</span>
+                </CardActions>
+            ),
+        },
+        {
+            name: "Reflect!",
+            image: ReflectCover,
+            description:
+                "Facilitating collaborative problem solving for small teams.",
+            chips: (
+                <CardActions className={classes.cardChips}>
+                    <span style={reflectChip}>UX Design + React JS</span>
+                    <span style={yearChip}>2018-2020</span>
+                </CardActions>
+            ),
+        },
+    ];
+
     return (
         <div style={{ padding: "1rem" }}>
-          <Typography variant="h4" component="h1" className={classes.title}>
+            <Typography variant="h4" component="h1" className={classes.title}>
                 projects
             </Typography>
             <div
@@ -74,102 +125,33 @@ const Projects = () => {
                     justify: "center",
                 }}
             >
-              
-                <Card className={classes.mainCard}>
-                    <CardMedia
-                        className={classes.cardImage}
-                        image={PECSSCover}
-                        title="PECSS Cover Photo"
-                    ></CardMedia>
-                    <CardContent>
-                        <Typography gutterBottom variant="h6" component="h2" className={classes.projectName}>
-                            PECSS Clinician Dashboard
-                        </Typography>
-                        <Typography
-                            variant="body2"
-                            color="textSecondary"
-                            component="p"
-                        >
-                            Prolonged Exposure Collective Sensing System.
-                            Improving PTSD treatment for veterans.
-                        </Typography>
-                    </CardContent>
-                    <CardActions className={classes.cardChips}>
-                      <span style={pecssChip}>UX Design + Angular JS</span>
-                      <span style={yearChip}>2019 - present</span>
-                    </CardActions>
-                </Card>
-                <Card className={classes.mainCard}>
-                    <CardMedia
-                        className={classes.cardImage}
-                        image={PEQCover}
-                        title="PEQ Cover Photo"
-                    ></CardMedia>
-                    <CardContent>
-                        <Typography gutterBottom variant="h6" component="h2" className={classes.projectName}>
-                            PEQ Widget
-                        </Typography>
-                        <Typography
-                            variant="body2"
-                            color="textSecondary"
-                            component="p"
-                        >
-                            Patient Engagement Quotient. Visualizing electronic personal health 
-                            information to improve patient adherence to treatment protocols.
-                        </Typography>
-                    </CardContent>
-                    <CardActions className={classes.cardChips}>
-                      <span style={peqChip}>UX Research + Design</span>
-                      <span style={yearChip}>2020 - present</span>
-                    </CardActions>
-                </Card>
-                <Card className={classes.mainCard}>
-                    <CardMedia
-                        className={classes.cardImage}
-                        image={KlixCover}
-                        title="Klix Cover Photo"
-                    ></CardMedia>
-                    <CardContent>
-                        <Typography gutterBottom variant="h6" component="h2" className={classes.projectName}>
-                            Klix
-                        </Typography>
-                        <Typography
-                            variant="body2"
-                            color="textSecondary"
-                            component="p"
-                        >
-                            Helping gamers find friends and schedule gameplay in
-                            a safe environment.
-                        </Typography>
-                    </CardContent>
-                    <CardActions className={classes.cardChips}>
-                      <span style={klixChip}>UX Research + Design</span>
-                      <span style={yearChip}>2019</span>
-                    </CardActions>
-                </Card>
-                <Card className={classes.mainCard}>
-                    <CardMedia
-                        className={classes.cardImage}
-                        image={ReflectCover}
-                        title="Reflect! Cover Photo"
-                    ></CardMedia>
-                    <CardContent>
-                        <Typography gutterBottom variant="h6" component="h2" className={classes.projectName}>
-                            Reflect!
-                        </Typography>
-                        <Typography
-                            variant="body2"
-                            color="textSecondary"
-                            component="p"
-                        >
-                            Facilitating collaborative problem solving for small teams. 
-                        </Typography>
-                    </CardContent>
-                    <CardActions className={classes.cardChips}>
-                      <span style={reflectChip}>UX Design + React JS</span>
-                      <span style={yearChip}>2018-2020</span>
-                    </CardActions>
-                </Card>
+                {projects.map((value) => (
+                    <Card className={classes.mainCard}>
+                        <CardMedia
+                            className={classes.cardImage}
+                            image={value.image}
+                            title={value.name}
+                        ></CardMedia>
+                        <CardContent>
+                            <Typography
+                                gutterBottom
+                                variant="h6"
+                                component="h2"
+                                className={classes.projectName}
+                            >
+                                {value.name}
+                            </Typography>
+                            <Typography
+                                variant="body2"
+                                color="textSecondary"
+                                component="p"
+                            >
+                                {value.description}
+                            </Typography>
+                        </CardContent>
+                        {value.chips}
+                    </Card>
+                ))}
             </div>
         </div>
     );
